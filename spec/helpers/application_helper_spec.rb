847c1e55e94a0c4cf328182b531cfa69639e1ec0
kot26-7 <taro726896oka@gmail.com>
2020-09-01 13:09:15 +0900

    create full_title method

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,9 @@
+  require 'rails_helper'
+
+RSpec.describe ApplicationHelper, type: :helper do
+  describe '#full_title helper method' do
+    it { expect(full_title(nil)).to eq 'My_app' }
+    it { expect(full_title('')).to eq 'My_app' }
+    it { expect(full_title('Home')).to eq 'Home - My_app' }
+  end
+end
