1b29a147e3f0e08b16235c93cd4806b54efc6599
kot26-7 <taro726896oka@gmail.com>
2020-09-01 17:25:18 +0900

    change name My app to Freedger

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
   devise_for :users
   root 'home#index'
   resources :users, except: [:new, :create]
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
   describe '#full_title helper method' do
-    it { expect(full_title(nil)).to eq 'My_app' }
-    it { expect(full_title('')).to eq 'My_app' }
-    it { expect(full_title('Home')).to eq 'Home - My_app' }
+    it { expect(full_title(nil)).to eq 'Freedger' }
+    it { expect(full_title('')).to eq 'Freedger' }
+    it { expect(full_title('Home')).to eq 'Home - Freedger' }
   end
 end
