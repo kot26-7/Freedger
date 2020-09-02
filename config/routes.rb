36e34d2d0aab1fd590b0a50be207944699c22b30
kot26-7 <taro726896oka@gmail.com>
2020-09-02 10:46:58 +0900

    create containers controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
-  resources :users, except: [:new, :create]
+  resources :users, except: [:new, :create] do
+    resources :containers
+  end
 end
