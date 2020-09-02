f4fe02a850915686faa562f50b07b078418dd0d5
kot26-7 <taro726896oka@gmail.com>
2020-09-02 18:44:01 +0900

    create product model

--- app/models/container.rb
+++ app/models/container.rb
@@ -1,5 +1,6 @@
 class Container < ApplicationRecord
   belongs_to :user
+  has_many :products, dependent: :destroy
   validates :name, presence: true, length: { maximum: 50 }
   validates :position, presence: true, length: { in: 6..7 }
   validates :description, length: { maximum: 150 }
