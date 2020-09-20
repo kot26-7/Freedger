0c7a6a7d50f0723572f4a8f694faa46c3c2c114e
kot26-7 <taro726896oka@gmail.com>
2020-09-20 12:13:51 +0900

    delete requests tests about home-about

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -19,21 +19,21 @@ RSpec.describe 'Home', type: :request do
     end
   end
 
-  describe 'GET home/about' do
-    before do
-      get about_path
-    end
+  # describe 'GET home/about' do
+  #   before do
+  #     get about_path
+  #   end
 
-    it 'returns http success' do
-      expect(response).to have_http_status(:success)
-    end
+  #   it 'returns http success' do
+  #     expect(response).to have_http_status(:success)
+  #   end
 
-    it 'renders the :about template' do
-      expect(response).to render_template :about
-    end
+  #   it 'renders the :about template' do
+  #     expect(response).to render_template :about
+  #   end
 
-    it 'displayed on correct title' do
-      expect(response.body).to include full_title('About')
-    end
-  end
+  #   it 'displayed on correct title' do
+  #     expect(response.body).to include full_title('About')
+  #   end
+  # end
 end
